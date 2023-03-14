dc853602914a180deda19d7e0b76c05663c4ef8d
Damian Tarnawsky <damian@ionic.io>
2023-03-14 06:15:09 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cs.start.anim',
+  appName: 'cs-start-anim',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
